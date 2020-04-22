9632e59e8169b89451df13b1bbb999878d84ff83
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-22 21:02:35 +0700

    contoh for mengisi gelas

--- src/Main.java
+++ src/Main.java
@@ -14,18 +14,32 @@ public class Main {
 
        // System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
 
-        int isiGelas = 0;
-        int penuh = 225;
+     //   int isiGelas = 0;
+     //   int penuh = 225;
 
-        System.out.println("Isi Gelas : " + isiGelas + "ml");
-        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+     //   System.out.println("Isi Gelas : " + isiGelas + "ml");
+     //   System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
 
-        do {
-            isiGelas++;
-            System.out.println("Sedang Mengisi Gelas..");
-            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-        }while(isiGelas != penuh);
+     //   do {
+     //       isiGelas++;
+     //       System.out.println("Sedang Mengisi Gelas..");
+     //       System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+     //   }while(isiGelas != penuh);
 
-        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+     //   System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+
+             int isiGelas = 0;
+             int penuh = 225;
+
+             System.out.println("Isi Gelas : " + isiGelas + "ml");
+             System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+             for(isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
+                 System.out.println("Sedang Mengisi Gelas..");
+                 System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+             };
+
+             System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+
     }
 }
