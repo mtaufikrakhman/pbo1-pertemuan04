1ae89179111853d58f2e000010e5bdb4b9ee28ef
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-22 20:57:12 +0700

    contoh do while mengisi gelas

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+      //  int isiGelas = 0;
+        // int penuh = 225;
+
+       // System.out.println("Isi Gelas : " + isiGelas + "ml");
+       // System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+       // while(isiGelas != penuh){
+          //  isiGelas++;
+          //  System.out.println("Sedang Mengisi Gelas..");
+          //  System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+      //  }
+
+       // System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+
         int isiGelas = 0;
         int penuh = 225;
 
         System.out.println("Isi Gelas : " + isiGelas + "ml");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
 
-        while(isiGelas != penuh){
+        do {
             isiGelas++;
             System.out.println("Sedang Mengisi Gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-        }
+        }while(isiGelas != penuh);
 
         System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
     }
