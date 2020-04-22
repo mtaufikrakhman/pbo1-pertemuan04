3f774cb00d5a90b91de6606437d2579856e0a44f
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-22 21:31:35 +0700

    contoh array multi dimensi nilai mahasiswa

--- src/Main.java
+++ src/Main.java
@@ -55,6 +55,39 @@ public class Main {
              mataKuliah[3] = "Organisasi Komputer";
              mataKuliah[4] = "Pemrograman Web 2";
 
+             String keterangan[] = {"Harian","Tugas","UTS","UAS"};
+
+             double bobot[] = {0.1, 0.2, 0.3, 0.4};
+
+             double[][] nilaiMahasiswa = new double[5][30];
+
+             nilaiMahasiswa[0][0] = 80;
+             nilaiMahasiswa[1][0] = 90;
+             nilaiMahasiswa[2][0] = 75;
+             nilaiMahasiswa[3][0] = 70;
+
+             nilaiMahasiswa[4][0] = nilaiMahasiswa[0][0] * bobot [0] + nilaiMahasiswa[1][0] * bobot [1] + nilaiMahasiswa[2][0] * bobot [2] + nilaiMahasiswa[3][0] * bobot [3];
+
+             System.out.println("Nilai Si Mahasiswa 1 :"+nilaiMahasiswa[4][0]);
+
+             nilaiMahasiswa[0][1] = 80;
+             nilaiMahasiswa[1][1] = 90;
+             nilaiMahasiswa[2][1] = 75;
+             nilaiMahasiswa[3][1] = 70;
+
+             nilaiMahasiswa[4][1] = 0;
+             for (int i=0 ; i<=3 ; i++){
+                 nilaiMahasiswa[4][1] += nilaiMahasiswa[i][1] * bobot[i];
+             }
+
+             System.out.println("Nilai Si Mahasiswa 2 :"+nilaiMahasiswa[4][1]);
+
+             double jumlahNilaiAkhir = 0;
+             for (int i=0 ; i<=3 ; i++) {
+                 jumlahNilaiAkhir += nilaiMahasiswa[4][i];
+             }
+             System.out.println(jumlahNilaiAkhir);
+
 
     }
 }
