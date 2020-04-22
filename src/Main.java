fb580098e1d7b0f56975c52c915185e7cea883c0
mtaufikrakhman <62371800+mtaufikrakhman@users.noreply.github.com>
2020-04-22 21:17:55 +0700

    contoh array mata kuliah

--- src/Main.java
+++ src/Main.java
@@ -41,5 +41,20 @@ public class Main {
 
              System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
 
+             int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
+             System.out.println(nilaiArr.length);
+             System.out.println(nilaiArr[0]);
+             System.out.println(nilaiArr[8]);
+
+             String mataKuliah[];
+             mataKuliah = new String[10];
+
+             mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+             mataKuliah[1] = "Visual 2";
+             mataKuliah[2] = "Rekayasa Perangkat Lunak";
+             mataKuliah[3] = "Organisasi Komputer";
+             mataKuliah[4] = "Pemrograman Web 2";
+
+
     }
 }
